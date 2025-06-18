ba53df6c58546826310c624c538d7b29fe947c77
PrathameshML <prathu004mali@gmail.com>
2025-06-18 16:28:07 +0530

    242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>mp;
+    
 
+        for(int i=0; i<s.size(); i++){
+            if(mp.find(s[i])!=mp.end()){
+                mp[s[i]]++;
+            }
+            else {
+                mp[s[i]]=1;
+            }
+        }
+
+         for(int i=0; i<t.size(); i++){
+           if( mp.find(t[i])!=mp.end()){
+                if(mp[t[i]]==0) return false;
+                else mp[t[i]]--;
+            }
+            else return false;
+         }
+         return true;
+
+    }
+};
